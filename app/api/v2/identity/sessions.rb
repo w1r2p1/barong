b6cd43b73f637eaf3ce017ad62d0ed3a16ba3d43
Vee <vee.jirapongcharoenlarb@yellow.com>
2021-01-25 19:21:37 +0700

    User can login with auth0

--- app/api/v2/identity/sessions.rb
+++ app/api/v2/identity/sessions.rb
@@ -2,32 +2,15 @@
 
 require_dependency 'barong/jwt'
 require 'net/http'
+require 'faraday'
+require 'uri'
 
 module API::V2
   module Identity
     class Sessions < Grape::API
-      desc 'Session related routes'
-      resource :sessions do
-        desc 'Start a new session',
-          failure: [
-            { code: 400, message: 'Required params are empty' },
-            { code: 404, message: 'Record is not found' }
-        ]
-        params do
-          requires :email
-          requires :password
-          optional :captcha_response,
-                   types: { value: [String, Hash], message: 'identity.session.invalid_captcha_format' },
-                   desc: 'Response from captcha widget'
-          optional :otp_code,
-                   type: String,
-                   desc: 'Code from Google Authenticator'
-        end
-        post do
-          verify_captcha!(response: params['captcha_response'], endpoint: 'session_create')
-
-          declared_params = declared(params, include_missing: false)
-          user = User.find_by(email: declared_params[:email])
+      helpers do
+        def get_user(email)
+          user = User.find_by(email: email)
           error!({ errors: ['identity.session.invalid_params'] }, 401) unless user
 
           if user.state == 'banned'
@@ -45,6 +28,31 @@ module API::V2
             login_error!(reason: 'Your account is not active', error_code: 401,
                          user: user.id, action: 'login', result: 'failed', error_text: 'not_active')
           end
+          user
+        end
+      end
+      desc 'Session related routes'
+      resource :sessions do
+        desc 'Start a new session',
+             failure: [
+               { code: 400, message: 'Required params are empty' },
+               { code: 404, message: 'Record is not found' }
+             ]
+        params do
+          requires :email
+          requires :password
+          optional :captcha_response,
+                   types: { value: [String, Hash], message: 'identity.session.invalid_captcha_format' },
+                   desc: 'Response from captcha widget'
+          optional :otp_code,
+                   type: String,
+                   desc: 'Code from Google Authenticator'
+        end
+        post do
+          verify_captcha!(response: params['captcha_response'], endpoint: 'session_create')
+
+          declared_params = declared(params, include_missing: false)
+          user = get_user(declared_params[:email])
 
           unless user.authenticate(declared_params[:password])
             login_error!(reason: 'Invalid Email or Password', error_code: 401, user: user.id,
@@ -75,10 +83,10 @@ module API::V2
         end
 
         desc 'Destroy current session',
-          failure: [
-            { code: 400, message: 'Required params are empty' },
-            { code: 404, message: 'Record is not found' }
-        ]
+             failure: [
+               { code: 400, message: 'Required params are empty' },
+               { code: 404, message: 'Record is not found' }
+             ]
         delete do
           user = User.find_by(uid: session[:uid])
           error!({ errors: ['identity.session.not_found'] }, 404) unless user
@@ -93,12 +101,12 @@ module API::V2
           desc 'Auth0 authentication',
                success: { code: 200, message: '' },
                failure: []
-          
+
           get '/authorize' do
             uri = URI::HTTPS.build(host: Barong::App.config.auth0_tenant_address, path: '/authorize', query: {
               response_type: 'code',
               client_id: Barong::App.config.client_id,
-              redirect_uri: 'http://localhost:3000' + Barong::App.config.redirect_uri,
+              redirect_uri: Barong::App.config.redirect_uri,
               scope: 'openid profile email',
               audience: Barong::App.config.audience
             }.to_query)
@@ -109,7 +117,32 @@ module API::V2
             requires :code
           end
           get '/callback' do
-            status(200)
+            url = "https://#{Barong::App.config.auth0_tenant_address}/oauth/token"
+            data = {
+              grant_type: 'authorization_code',
+              client_id: Barong::App.config.client_id,
+              client_secret: Barong::App.config.client_secret,
+              code: params[:code],
+              redirect_uri: Barong::App.config.redirect_uri
+            }
+            response = Faraday.post(url) do |req|
+              req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+              req.body = URI.encode_www_form(data)
+            end
+            if response.status == 200
+              json = JSON.parse(response.body)
+              id_token = json['id_token']
+              claims = JWT.decode(id_token, nil, false, algorithm: 'RS256')
+              user = get_user(claims[0]['email'])
+              
+              activity_record(user: user.id, action: 'login::2fa', result: 'succeed', topic: 'session')
+              csrf_token = open_session(user)
+              publish_session_create(user)
+
+              present user, with: API::V2::Entities::UserWithFullInfo, csrf_token: csrf_token
+              status(200)
+            end
+            status(response.status)
           end
         end
       end
--- config/initializers/barong.rb
+++ config/initializers/barong.rb
@@ -116,7 +116,8 @@ Barong::App.define do |config|
   # Auth0 configuration -----------------------------------------------
   config.set(:auth0_tenant_address, 'openware-barong.us.auth0.com')
   config.set(:client_id, 've6NAufF1Pgm1jIhyQgEwa8PCG4vAoNF')
-  config.set(:redirect_uri, '/api/v2/identity/sessions/auth0/callback')
+  config.set(:client_secret, 'ynaE7yyYjvnuhFN_3bx435H2v0IF8jLQpwUCW8ovj-6ONRefuN7ker2L_RR5VfCI')
+  config.set(:redirect_uri, 'http://localhost:3000/api/v2/identity/sessions/auth0/callback')
   config.set(:audience, 'https://barong.openware.service/')
 end
 
